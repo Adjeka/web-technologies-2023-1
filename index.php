3fca4812b852de5122608b9ced6221410c6aabbf
Adjeka <130905244+Adjeka@users.noreply.github.com>
2024-06-08 04:16:57 +0500

    lesson17 done

--- index.php
+++ index.php
@@ -1,50 +1,117 @@
 <?php
-$page_title = "Главная страница";
-$page_h1 = "Год и время";
-$current_year = date("Y");
-$current_time = getCurrentTime();
+echo "<br>Задание 1<br>";
+$a = 1;
+$b = -1;
 
-function GetCurrentTime(): string
-{
-    $hours = date("G");
-    $minutes = date("i");
-
-    if ($hours == 1 || $hours == 21) {
-        $hour_suffix = "час";
-    } elseif (($hours >= 2 && $hours <= 4) || ($hours >= 22 && $hours <= 23)) {
-        $hour_suffix = "часа";
-    } else {
-        $hour_suffix = "часов";
-    }
-
-    if ($minutes == 1 || $minutes == 21 || $minutes == 31 || $minutes == 41 || $minutes == 51) {
-        $minute_suffix = "минута";
-    } elseif (($minutes >= 2 && $minutes <= 4) || 
-        ($minutes >= 22 && $minutes <= 24) || 
-        ($minutes >= 32 && $minutes <= 34) || 
-        ($minutes >= 42 && $minutes <= 44) || 
-        ($minutes >= 52 && $minutes <= 54)) {
-        $minute_suffix = "минуты";
-    } else {
-        $minute_suffix = "минут";
-    }
-
-    return "$hours $hour_suffix $minutes $minute_suffix";
+if ($a >= 0 && $b >= 0) {
+    echo "Разность:" . ($a - $b) . "<br>";
+} elseif ($a < 0 && $b < 0) {
+    echo "Произведение:" . ($a * $b) . "<br>";
+} else {
+    echo "Сумма: " . ($a + $b) . "<br>";
 }
-?>
 
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $page_title; ?></title>
-</head>
-<body>
-<div id="content">
-    <h1><?php echo $page_h1; ?></h1>
-    <p>Текущий год: <?php echo $current_year; ?></p>
-    <p>Текущее время: <?php echo $current_time; ?></p>
-</div>
-</body>
-</html>
+echo "<br>Задание 2<br>";
+$a = 12;
+
+switch ($a) {
+    case 0:
+        echo "0<br>";
+    case 1:
+        echo "1<br>";
+    case 2:
+        echo "2<br>";
+    case 3:
+        echo "3<br>";
+    case 4:
+        echo "4<br>";
+    case 5:
+        echo "5<br>";
+    case 6:
+        echo "6<br>";
+    case 7:
+        echo "7<br>";
+    case 8:
+        echo "8<br>";
+    case 9:
+        echo "9<br>";
+    case 10:
+        echo "10<br>";
+    case 11:
+        echo "11<br>";
+    case 12:
+        echo "12<br>";
+    case 13:
+        echo "13<br>";
+    case 14:
+        echo "14<br>";
+    case 15:
+        echo "15<br>";
+        break;
+    default:
+        echo "Значение вне диапазона [0..15]<br>";
+}
+
+echo "<br>Задание 3<br>";
+function addition($a, $b) {
+    return $a + $b;
+}
+echo addition(5,5) ."<br>";
+
+function subtraction($a, $b) {
+    return $a - $b;
+}
+echo subtraction(5,5) ."<br>";
+
+function multiplication($a, $b) {
+    return $a * $b;
+}
+echo multiplication(5,5) ."<br>";
+
+function division($a, $b) {
+    if ($b != 0) {
+        return $a / $b;
+    } else {
+        return "Делить на ноль нельзя!";
+    }
+}
+echo division(5,5) ."<br>";
+
+echo "<br>Задание 4<br>";
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case 'addition':
+            return addition($arg1, $arg2);
+        case 'subtraction':
+            return subtraction($arg1, $arg2);
+        case 'multiplication':
+            return multiplication($arg1, $arg2);
+        case 'division':
+            return division($arg1, $arg2);
+        default:
+            return "Неизвестная операция";
+    }
+}
+echo mathOperation(5,5,"addition") ."<br>";
+echo mathOperation(5,5,"subtraction") ."<br>";
+echo mathOperation(5,5,"multiplication") ."<br>";
+echo mathOperation(5,5,"division") ."<br>";
+
+echo "<br>Задание 5<br>";
+echo "Текущий год: " . date("Y") . "<br>";
+echo "Текущий год: " . getdate()["year"] . "<br>";
+echo "Текущий год: " . date_create()->format("Y") . "<br>";
+
+echo "<br>Задание 6<br>";
+function power($val, $pow) {
+    if ($pow == 0) {
+        return 1;
+    } elseif ($pow > 0) {
+        return $val * power($val, $pow - 1);
+    } else {
+        return 1 / power($val, -$pow);
+    }
+}
+
+echo  power(3,3) . "<br>"
+?>
