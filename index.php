f630d229d7efccfe32bc5ed3a46f5eba1ad71b30
Adjeka <130905244+Adjeka@users.noreply.github.com>
2024-06-08 06:16:46 +0500

    lesson18 done

--- index.php
+++ index.php
@@ -1,117 +1,84 @@
 <?php
 echo "<br>Задание 1<br>";
-$a = 1;
-$b = -1;
-
-if ($a >= 0 && $b >= 0) {
-    echo "Разность:" . ($a - $b) . "<br>";
-} elseif ($a < 0 && $b < 0) {
-    echo "Произведение:" . ($a * $b) . "<br>";
-} else {
-    echo "Сумма: " . ($a + $b) . "<br>";
-}
+$i = 0;
+do {
+    if ($i == 0) {
+        echo "$i – это ноль.<br>";
+    } elseif ($i % 2 == 0) {
+        echo "$i – чётное число.<br>";
+    } else {
+        echo "$i – нечётное число.<br>";
+    }
+    $i++;
+} while ($i <= 10);
 
 echo "<br>Задание 2<br>";
-$a = 12;
-
-switch ($a) {
-    case 0:
-        echo "0<br>";
-    case 1:
-        echo "1<br>";
-    case 2:
-        echo "2<br>";
-    case 3:
-        echo "3<br>";
-    case 4:
-        echo "4<br>";
-    case 5:
-        echo "5<br>";
-    case 6:
-        echo "6<br>";
-    case 7:
-        echo "7<br>";
-    case 8:
-        echo "8<br>";
-    case 9:
-        echo "9<br>";
-    case 10:
-        echo "10<br>";
-    case 11:
-        echo "11<br>";
-    case 12:
-        echo "12<br>";
-    case 13:
-        echo "13<br>";
-    case 14:
-        echo "14<br>";
-    case 15:
-        echo "15<br>";
-        break;
-    default:
-        echo "Значение вне диапазона [0..15]<br>";
+$regions = [
+    "Московская область" => ["Москва", "Зеленоград", "Клин"],
+    "Ленинградская область" => ["Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"],
+    "Рязанская область" => ["Рязань", "Скопин", "Сасово"]
+];
+foreach ($regions as $region => $cities) {
+    echo "$region: " . implode(", ", $cities) . ".<br>";
 }
 
 echo "<br>Задание 3<br>";
-function addition($a, $b) {
-    return $a + $b;
-}
-echo addition(5,5) ."<br>";
+$cyrillicToLatin = [
+    'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 
+    'д' => 'd', 'е' => 'e', 'ё' => 'e', 'ж' => 'zh', 
+    'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 
+    'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 
+    'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 
+    'у' => 'u', 'ф' => 'f', 'х' => 'kh', 'ц' => 'c', 
+    'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '``', 
+    'ы' => 'y', 'ь' => '`', 'э' => 'e`', 'ю' => 'yu', 'я' => 'ya'
+];
 
-function subtraction($a, $b) {
-    return $a - $b;
-}
-echo subtraction(5,5) ."<br>";
-
-function multiplication($a, $b) {
-    return $a * $b;
-}
-echo multiplication(5,5) ."<br>";
-
-function division($a, $b) {
-    if ($b != 0) {
-        return $a / $b;
-    } else {
-        return "Делить на ноль нельзя!";
+function transliteration($str, $cyrillicToLatin) {
+    $result = '';
+    $str = mb_strtolower($str);
+    $charArray = mb_str_split($str);
+    for ($i = 0; $i < mb_strlen($str); $i++) {
+        //$char = mb_substr($str, $i, 1);
+        $char = $charArray[$i];
+        if (isset($cyrillicToLatin[$char])) {
+            $result .= $cyrillicToLatin[$char];
+        } else {
+            $result .= $char;
+        }
     }
+    return $result;
 }
-echo division(5,5) ."<br>";
+echo transliteration("Задание 3", $cyrillicToLatin) . "<br>";
 
 echo "<br>Задание 4<br>";
-function mathOperation($arg1, $arg2, $operation) {
-    switch ($operation) {
-        case 'addition':
-            return addition($arg1, $arg2);
-        case 'subtraction':
-            return subtraction($arg1, $arg2);
-        case 'multiplication':
-            return multiplication($arg1, $arg2);
-        case 'division':
-            return division($arg1, $arg2);
-        default:
-            return "Неизвестная операция";
+function renderMenu($menu) {
+    echo "<ul>";
+    foreach ($menu as $key => $value) {
+        if (is_array($value)) {
+            echo "<li>$key";
+            renderMenu($value);
+            echo "</li>";
+        } else {
+            echo "<li><a href=\"$key\">$value</a></li>";
+        }
     }
+    echo "</ul>";
 }
-echo mathOperation(5,5,"addition") ."<br>";
-echo mathOperation(5,5,"subtraction") ."<br>";
-echo mathOperation(5,5,"multiplication") ."<br>";
-echo mathOperation(5,5,"division") ."<br>";
 
-echo "<br>Задание 5<br>";
-echo "Текущий год: " . date("Y") . "<br>";
-echo "Текущий год: " . getdate()["year"] . "<br>";
-echo "Текущий год: " . date_create()->format("Y") . "<br>";
+renderMenu($regions);
+
 
 echo "<br>Задание 6<br>";
-function power($val, $pow) {
-    if ($pow == 0) {
-        return 1;
-    } elseif ($pow > 0) {
-        return $val * power($val, $pow - 1);
-    } else {
-        return 1 / power($val, -$pow);
+foreach ($regions as $region => $cities) {
+    $citiesOnK = [];
+    foreach ($cities as $city) {
+        if (mb_substr($city,0,1) == 'К') {
+            $citiesOnK[] = $city;
+        }
+    }
+    if (count($citiesOnK) != 0) {
+        echo "$region: " . implode(", ", $citiesOnK) . ".<br>";
     }
 }
-
-echo  power(3,3) . "<br>"
 ?>
